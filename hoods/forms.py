47238fb62989ca635de6bd1748831347e5983eee
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-04-12 17:44:26 +0300

    forms addition

--- hoods/forms.py
+++ hoods/forms.py
@@ -1,3 +1,18 @@
 from django import forms
 from django.forms import ModelForm
 from .models import NeighbourHood, Business, Profile
+
+class NeighbourHoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighbourHood
+        fields =('name','location','photo')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('user','profile_photo','email','estate')
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        fields = ('name','description','business_photo','location')
