805b8101890637c948e4219df5cd3181365b1f51
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-04-14 11:42:34 +0300

    changes

--- Neigh/settings.py
+++ Neigh/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST':'',
+        'HOST':os.environ.get('DB_HOST'),
         'PORT':os.environ.get('DB_PORT')
     }
 }
